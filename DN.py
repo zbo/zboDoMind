ae0f3dc8006872f16abdac79b45d55ce6e1822b9
zhubo <zhubo@microstrategy.com>
2020-06-23 16:20:04 +0000

    update

--- DN.py
+++ DN.py
@@ -34,35 +34,49 @@ if __name__ == '__main__':
     zrzt = int(webget("非st，非新股，昨日涨停"))
     zrzt_hp = int(webget("非st，非新股，昨日涨停, 今日上涨"))
     score1 = score(zrzt * 0.6 < zrzt_hp)
-    print "[昨日涨停上涨60%+] 昨日涨停:{0} 今天上涨:{1} 得分:{2}".format(zrzt, zrzt_hp, score1)
+    print "[昨日涨停上涨60%+] 昨日涨停:{0} 今天上涨:{1} 占比{2}% 得分:{3}".format(zrzt, zrzt_hp, zrzt_hp*100/zrzt, score1)
 
     # 昨日连续涨停今日红盘比 大于 0.6 +2分
     zrlxzt = int(webget("非st，非新股，昨日连板"))
     zrlxzt_hp = int(webget("非st，非新股，昨日连板, 今日上涨"))
     score2 = score(zrlxzt * 0.6 < zrlxzt_hp)
-    print "[昨日连板上涨60%+] 昨日连板:{0} 今天上涨:{1} 得分:{2}".format(zrlxzt, zrlxzt_hp, score2)
+    print "[昨日连板上涨60%+] 昨日连板:{0} 今天上涨:{1} 占比{2}% 得分:{3}".format(zrlxzt, zrlxzt_hp, zrlxzt_hp*100/zrlxzt, score2)
 
     # 昨日断板绿盘比例 小于 0.4 +2分
     lp = int(webget("非st，非新股，周五未能涨停，周四涨停"))
     lp2 = int(webget("非st，非新股，周五未能涨停，周四涨停， 今天下跌"))
     score5 = score(lp2 < lp * 0.4)
-    print "[昨日断板下跌40%-] 昨日断板:{0} 断板后今天下跌:{1} 得分:{2}".format(lp, lp2, score5)
+    print "[昨日断板下跌] 昨日断板:{0} 断板后今天下跌:{1} 占比{2}% 得分:{3}".format(lp, lp2, lp2*100/lp, score5)
 
     # 昨日涨停今日跌幅超4%比例 小于 0.4 +2分
     zrzt_dm = int(webget("非st，非新股，昨日涨停, 今日跌幅大于4%"))
     score3 = score(zrzt_dm < zrzt*0.4)
-    print "[昨日涨停大面40%-] 昨日涨停:{0} 今天大面:{1} 得分:{2}".format(zrzt, zrzt_dm, score3)
+    print "[昨日涨停大面] 昨日涨停:{0} 今天大面:{1} 占比{2}% 得分:{3}".format(zrzt, zrzt_dm, zrzt_dm*100/zrzt, score3)
 
     # 昨日连板涨停今日跌幅超4%比例 小于 0.4 +2分
     zrlxzt_dm = int(webget("非st，非新股，昨日连板, 今日跌幅大于4%"))
     score4 = score(zrlxzt_dm < zrlxzt * 0.4)
-    print "[昨日连板大面40%-] 昨日连续涨停:{0} 今天大面:{1} 得分:{2}".format(zrlxzt, zrlxzt_dm, score4)
+    print "[昨日连板大面] 昨日连续涨停:{0} 今天大面:{1} 占比{2}% 得分:{3}".format(zrlxzt, zrlxzt_dm, zrlxzt_dm*100/zrlxzt, score4)
 
-    # 今日炸板和涨停比 小于 40% +2分
+    # 今日炸板和涨停比 小于 30% +2分
     zb = int(webget("非st，非新股，今日炸板"))
     zt = int(webget("非st，非新股，今日涨停"))
-    score6 = score(zb < zt*0.4)
-    print "[今日炸板40%-] 炸板:{0} 涨停:{1} 得分:{2}".format(zb,zt,score6)
+    score6 = score(zb < zt*0.3)
+    print "[今日炸板] 炸板:{0} 涨停:{1} 得分:{2}".format(zb,zt,score6)
     print "---------------"
     print "燃料动能{0}".format(score1 + score2 + score3 + score4 + score5 + score6)
     print "---------------"
+
+    # 其他
+    zbdm = int(webget("非st，非新股，昨日炸板，今日下跌超过4%"))
+    zbsz = int(webget("非st，非新股，昨日炸板，今日上涨"))
+    print "[昨日炸板大面比] 昨日炸板{0} 今日上涨{1} 占比{2}% 大面{3} 占比{4}%".format(zb,zbsz, zbsz*100/zb, zbdm, zbdm*100/zb)
+
+    gn_list = ["头条概念","室外经济概念","免税概念","芯片概念","网络游戏概念","特斯拉概念"]
+    gndm_tmp = "非st，非新股，今日跌幅大于4%, {0}"
+    gndz_tmp = "非st，非新股，今日涨幅大于4%, {0}"
+    for g in gn_list:
+        gn = int(webget(g))
+        gndm = int(webget(gndm_tmp.format(g)))
+        gndz = int(webget(gndz_tmp.format(g)))
+        print "[{0}]总量{1} 大涨{2} 占比{3}% 大面{4} 占比{5}%".format(g,gn,gndz,gndz*100/gn,gndm,gndm*100/gn)
--- SNDN.sh
+++ SNDN.sh
@@ -1,4 +1,4 @@
-cd /Users/zhu/bob/zboDoMind/
+ps aux |grep chrome|awk '{print $2}'|xargs -i kill {}
 /usr/bin/python SN.py
 /usr/bin/python DN.py
 /usr/bin/python WC.py
