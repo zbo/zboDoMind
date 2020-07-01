f1be6ebe0399af1b6e94967a0008a9767d6eb277
zhubo <zhubo@microstrategy.com>
2020-07-01 08:58:23 +0000

    save

--- WC.py
+++ WC.py
@@ -26,25 +26,25 @@ if __name__ == '__main__':
     browser = webdriver.Chrome(chrome_options=options)
     sz = webget('{0}上涨'.format(jrday))
     xd = webget('{0}下跌'.format(jrday))
+    print '上涨:{sz} 下跌:{xd}'.format(sz=sz, xd=xd)
     sz5 = webget('{0}上涨大于5%'.format(jrday))
     xd5 = webget('{0}下跌大于5%'.format(jrday))
+    print '上涨大于5%:{sz5} 下跌大于5%:{xd5} \n'.format(sz5=sz5, xd5=xd5)
     zt = webget('非st,{0}涨停'.format(jrday))
     dt = webget('非st,{0}跌停'.format(jrday))
+    print '涨停:{zt} 跌停:{dt}'.format(zt=zt, dt=dt)
     zb = webget('非st,{0}炸板'.format(jrday))
-    fb = webget('反包, {0}下跌大于3%'.format(jrday))
+    fb = webget('反包, {0}下跌大于3%'.format(zrday))
+    print '今日反包:{fb} 今日炸板:{zb}'.format(fb=fb, zb=zb)
+    zrzb = webget('非st,{0}炸板'.format(zrday))
+    jrsz = webget('非st,{0}炸板,{1}上涨'.format(zrday, jrday))
+    print '昨日炸板{0},今日上涨{1}'.format(zrzb,jrsz)
     llb = webget('非st,非新股,{0},两连板'.format(jrday))
     slb = webget('非st,非新股,{0},三连板'.format(jrday))
     lb4 = webget('非st,非新股,{0},四连板'.format(jrday))
     lb5 = webget('非st,非新股,{0},五连板'.format(jrday))
     lb6 = webget('非st,非新股,{0},六连板'.format(jrday))
-    lb7 = webget('非st,非新股,{0},七连板以上'.format(jrday))
-    ret_list = ['上涨:{sz} 下跌:{xd} \n'.format(sz=sz, xd=xd),
-                '上涨大于5%:{sz5} 下跌大于5%:{xd5} \n'.format(sz5=sz5, xd5=xd5),
-                '涨停:{zt} 跌停:{dt} \n'.format(zt=zt, dt=dt),
-                '反包:{fb} 炸板:{zb} \n'.format(fb=fb, zb=zb),
-                '两连板:{llb} 三连板:{slb} \n'.format(llb=llb, slb=slb),
-                '四连板:{0} 五连板:{1} 六连板:{2} 七连板以上:{3}\n'.format(lb4,lb5,lb6,lb7)]
-    content = "".join(ret_list)
-    file_object = open('today.log', 'w')
-    file_object.writelines(content)
-    file_object.close()
+    lb7 = webget('非st,非新股,{0},七连板以上'.format(jrday))         
+    print '两连板:{llb} 三连板:{slb}'.format(llb=llb, slb=slb)
+    print '四连板:{0} 五连板:{1} 六连板:{2} 七连板以上:{3}'.format(lb4,lb5,lb6,lb7)
+    
--- WC.sh
+++ WC.sh
@@ -1,2 +1,1 @@
 /usr/bin/python WC.py
-cat today.log
